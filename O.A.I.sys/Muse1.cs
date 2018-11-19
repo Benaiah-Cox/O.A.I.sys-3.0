456503fa84d8d1864a632b7fa8258936184c4935
benaiah cox <benaiahcox@hotmail.com>
2018-11-18 20:49:13 -0500

    MORE NOTES

--- O.A.I.sys/Muse1.cs
+++ O.A.I.sys/Muse1.cs
@@ -14,15 +14,23 @@ namespace O.A.I.sys
         string[] FM  = new string[8] {"F","G","A","Bb","C","D","E","F2"};  
         string[] GM  = new string[8] {"G","A","B","C","D","E","F#","G2"}; 
         string[] AM  = new string[8] {"A","B","C#","D","E","F#","G#","A2"}; 
-        string[] Bm = new string[8] {"","","","","","","","2"}; 
-        string[]   = new string[8] {"","","","","","","","2"}; 
-        string[]   = new string[8] {"","","","","","","","2"}; 
-        string[]   = new string[8] {"","","","","","","","2"}; 
-        string[]   = new string[8] {"","","","","","","","2"}; 
-        string[]   = new string[8] {"","","","","","","","2"}; 
-        string[]   = new string[8] {"","","","","","","","2"}; 
-        string[]   = new string[8] {"","","","","","","","2"}; 
-        string[]   = new string[8] {"","","","","","","","2"}; 
-        string[]   = new string[8] {"","","","","","","","2"};  
+        string[] Bm = new string[8] {"B","C#","D#","E","F#","G#","A#","B2"}; 
+        string[] CsDb = new string[8] {"Db","Eb","F","Gb","Ab","Bb","C","Db2"}; 
+        string[] DsEb = new string[8] {"Eb","F","G","Ab","Bb","C","D","Eb2"}; 
+        string[] FsGb  = new string[8] {"F#","G#","A#","B","C#","D#","F","F#2"}; 
+        string[] GsAb  = new string[8] {"Ab","Bb","C","Db","Eb","F","G","Ab2"}; 
+        string[] AsBb  = new string[8] {"Bb","C","D","Eb","F","G","A","Bb2"}; 
+        string[] Am  = new string[8] {"A","B","C","D","E","F","G","A2"}; 
+        string[] AsBbm  = new string[8] {"A#","C","C#","D#","F","F#","G#","A#2"};  
+        string[] Cm  = new string[8] {"C","D","Db","F","G","Ab","Bb","C2"};
+        string[] CsDbm  = new string[8] {"C#","D#","E","F#","G#","A","B","C#2"};
+        string[] Dm = new string[8] {"D","E","F","G","A","Bb","C","D2"};
+        string[] DsEbm = new string[8] {"D#","F","F#","G#","A#","B","C#","D#2"};
+        string[] Em  = new string[8] {"","","","","","","","2"};
+        string[]   = new string[8] {"","","","","","","","2"};
+        string[]   = new string[8] {"","","","","","","","2"};
+        string[]   = new string[8] {"","","","","","","","2"};
+        string[]   = new string[8] {"","","","","","","","2"};
+        string[]   = new string[8] {"","","","","","","","2"};
     }
 }
