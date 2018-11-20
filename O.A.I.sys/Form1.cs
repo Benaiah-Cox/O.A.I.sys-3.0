a213c18586042ca67ddfc2776d11fad0d1dfb823
benaiah cox <benaiahcox@hotmail.com>
2018-11-20 00:38:53 -0500

    ch2

--- O.A.I.sys/Form1.cs
+++ O.A.I.sys/Form1.cs
@@ -18,7 +18,29 @@ namespace O.A.I.sys
         SpeechSynthesizer s = new SpeechSynthesizer();
 
         string Password = "user1";
-
+        string[] CM = new string[8] { "C", "D", "E", "F", "G", "A", "B", "C2" };
+        string[] DM = new string[8] { "D", "E", "F#", "G", "A", "B", "C#", "D2" };
+        string[] EM = new string[8] { "E", "F#", "G#", "A", "B", "C#", "D#", "E2" };
+        string[] FM = new string[8] { "F", "G", "A", "Bb", "C", "D", "E", "F2" };
+        string[] GM = new string[8] { "G", "A", "B", "C", "D", "E", "F#", "G2" };
+        string[] AM = new string[8] { "A", "B", "C#", "D", "E", "F#", "G#", "A2" };
+        string[] Bm = new string[8] { "B", "C#", "D#", "E", "F#", "G#", "A#", "B2" };
+        string[] CsDb = new string[8] { "Db", "Eb", "F", "Gb", "Ab", "Bb", "C", "Db2" };
+        string[] DsEb = new string[8] { "Eb", "F", "G", "Ab", "Bb", "C", "D", "Eb2" };
+        string[] FsGb = new string[8] { "F#", "G#", "A#", "B", "C#", "D#", "F", "F#2" };
+        string[] GsAb = new string[8] { "Ab", "Bb", "C", "Db", "Eb", "F", "G", "Ab2" };
+        string[] AsBb = new string[8] { "Bb", "C", "D", "Eb", "F", "G", "A", "Bb2" };
+        string[] Am = new string[8] { "A", "B", "C", "D", "E", "F", "G", "A2" };
+        string[] AsBbm = new string[8] { "A#", "C", "C#", "D#", "F", "F#", "G#", "A#2" };
+        string[] Cm = new string[8] { "C", "D", "Db", "F", "G", "Ab", "Bb", "C2" };
+        string[] CsDbm = new string[8] { "C#", "D#", "E", "F#", "G#", "A", "B", "C#2" };
+        string[] Dm = new string[8] { "D", "E", "F", "G", "A", "Bb", "C", "D2" };
+        string[] DsEbm = new string[8] { "D#", "F", "F#", "G#", "A#", "B", "C#", "D#2" };
+        string[] Em = new string[8] { "E", "F#", "G", "A", "B", "C", "D", "E2" };
+        string[] Fm = new string[8] { "F", "G", "Ab", "Bb", "C", "Db", "Eb", "F2" };
+        string[] FsGbm = new string[8] { "F#", "G#", "A", "B", "C#", "D", "E", "F2" };
+        string[] Gm = new string[8] { "G", "A", "Bb", "C", "D", "Eb", "F", "G2" };
+        string[] GsAbm = new string[8] { "G#", "A#", "B", "C#", "D#", "Eb", "F#", "G#2" };
 
         /// <summary>
         /// CONTROLS SLEEP AND WAKE MODE SO ThAT it Doesnt RECOGNISE UNWANTED COMMANDS
@@ -32,7 +54,7 @@ namespace O.A.I.sys
             SpeechRecognitionEngine rec = new SpeechRecognitionEngine();
             /// ALWAAYS ADD NEW COMMANDS HERE THIS IS A LIST OF VIABLE COMMANDS KEEP LOWER CASE
 
-            list.Add(new String[] {"restart", "open music ","lets order pizza", "hello", "how are you", "What time is it", "What day is it", "I need to search the web", "open google", "wake up", "sleep", "caip", "go to sleep" });
+            list.Add(new String[] { "restart", "open music ", "lets order pizza", "lets make a song", "hello", "how are you", "What time is it", "What day is it", "I need to search the web", "open google", "wake up", "sleep", "caip", "go to sleep" });
 
             Grammar gr = new Grammar(new GrammarBuilder(list));
 
@@ -54,12 +76,12 @@ namespace O.A.I.sys
 
             InitializeComponent();
         }
-      ///  public void restart()
-     //   {
-      //      Process.Start(c\)
-      //  }
-    /// <summary>
-   
+        ///  public void restart()
+        //   {
+        //      Process.Start(c\)
+        //  }
+        /// <summary>
+
         public void say(String h)
         {
             s.Speak(h);
@@ -72,7 +94,7 @@ namespace O.A.I.sys
         {
             String r = e.Result.Text;
 
-           
+
 
             if (r == "sleep" || r == "go to sleep") wake = false;
             if (wake == false)
@@ -83,8 +105,8 @@ namespace O.A.I.sys
             if (wake == true)
             {
 
-                
-               
+
+
 
                 // What You Say (ALWAYS KEEP LOWER CASE)
                 if (r == "hello")
@@ -115,18 +137,58 @@ namespace O.A.I.sys
                 if (r == "I need to search the web" || r == "open google")
                 {
                     Process.Start("http://www.google.com/‎");
-                      
-                        
+
+
+
+                }
+
+                if (r == "lets make a song")
+                {
+                    Console.WriteLine(" please enter a key  " +
+                        "CM = c maj " +
+                        "DM = d maj " +
+                        "EM = e maj" +
+                        "FM =  f maj" +
+                        "GM = g maj  " +
+                        "AM = a maj  " +
+                        "Bm = b maj  " +
+                        "CsDb c sharp / d flat " +
+                        "DsEb d sharp / e flat" +
+                        "FsGb f sharp / g flat " +
+                        "GsAb g sharp / a flat " +
+                        "AsBb a sharp / b flat " +
+                        "Am = a minor " +
+                        "AsBbm a sharp / b flat minor " +
+                        "Cm = C minor " +
+                        "CsDbm c sharp / d flat minor" +
+                        "Dm = d minor " +
+                        "DsEbm = d sharp / e flat minor  " +
+                        "Em = e minor " +
+                        "Fm =  f minor" +
+                        "FsGbm f sharp / g flat major" +
+                        "Gm = g minor " +
+                        "GsAbm  = g sharp / a flat minor" );
+
+                    string reeeet = Console.ReadLine () ;
+                   
+
+                    if (reeeet == "CM")
+                    {
+
+                         string[] keysr = KeyReturn(CM);
+                        Console.WriteLine(keysr.ToString());
+
+                    }
+
 
                 }
 
 
-                
 
 
                 if (r == "lets order pizza")
                 {
-                   Process.Start(" http://www.papajohns.com‎/");
+                    Process.Start(" http://www.papajohns.com‎/");
 
                     say("okay, lets see whats on the menu, I like pepperoni");
 
@@ -141,5 +203,19 @@ namespace O.A.I.sys
         {
 
         }
+
+
+        public static string[] KeyReturn(string[] a)
+        {
+
+            string[] keys = a;
+
+
+            return keys;
+
+        }
     }
+
+   
+
 }
